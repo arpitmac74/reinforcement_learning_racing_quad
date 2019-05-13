5caaac04e22a0fc85af0daf56317284e88185e0f
ASHWIN VARGHESE KURUTTUKULAM <ashwinvk94@gmail.com>
2019-05-13 19:59:01 -0400

    Final push from Robot Realization Lab

---
+++ scripts/2_gate_rate_thrust_publisher.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python2
+
+'''
+This file just publishes input from joy to quad. (Direct rate publisher).
+'''
+
+import rospy
+from mav_msgs.msg import RateThrust
+from sensor_msgs.msg import Joy,Imu
+import tf.transformations
+import sys
+from keras.models import load_model
+from flightgoggles.msg import IRMarkerArray
+import numpy as np
+
+
+class test:
+    def __init__(self):
+        # Rate init
+        # DECIDE ON PUBLISHING RATE
+        self.rate = rospy.Rate(120.0)  # MUST be more then 2Hz
+        self.joy_cb_flag = False
+        self.imu_cb_flag = False
+        self.attitude_thrust_pub = rospy.Publisher("/uav/input/rateThrust", RateThrust, queue_size=10)
+        self.model = load_model('working_model_roll_pitch_thrust.h5')
+        self.model._make_predict_function()
+        self.counter = 1
+        attitude_target_sub = rospy.Subscriber("/joy",Joy,self.joy_cb)
+        attitude_target_sub = rospy.Subscriber("/uav/sensors/imu",Imu,self.imu_cb)
+        attitude_target_sub = rospy.Subscriber("/uav/camera/left/ir_beacons",IRMarkerArray,self.gate_cb)
+        self.attitude_thrust_pub = rospy.Publisher("/uav/input/rateThrust", RateThrust, queue_size=10)
+
+        thrust_scale = 2
+        ang_scale = 1
+        counter = 0
+        while not rospy.is_shutdown():
+            # if(self.joy_cb_flag == True):
+            target_attitude_thrust = RateThrust()
+            target_attitude_thrust.header.frame_id = "home"
+            target_attitude_thrust.header.stamp = rospy.Time.now()
+            target_attitude_thrust.angular_rates.x = 0
+            target_attitude_thrust.angular_rates.y = 0
+            target_attitude_thrust.angular_rates.z = 0
+            target_attitude_thrust.thrust.z = 10.5
+            self.attitude_thrust_pub.publish(target_attitude_thrust)
+            self.rate.sleep()
+            counter += 1
+            if counter>20:
+                break
+
+    def joy_cb(self, state):
+        #print(state)
+        axes = state.axes
+        #RRL
+        self.thrust = axes[1]
+        self.yaw = axes[0]
+        self.pitch = axes[4]
+        self.roll = axes[3]
+        #ARC
+        # self.thrust = axes[1]
+        # self.yaw = axes[0]
+        # self.pitch = axes[3]
+        # self.roll = axes[2]
+        self.joy_cb_flag = True
+
+    def imu_cb(self, state):
+        #print(state)
+        self.ang_x = state.angular_velocity.x
+        self.ang_y = state.angular_velocity.y
+        self.ang_z = state.angular_velocity.z
+        self.lin_x = state.linear_acceleration.x
+        self.lin_y = state.linear_acceleration.y
+        self.lin_z = state.linear_acceleration.z
+        self.imu_cb_flag = True
+
+    def gate_cb(self,state):
+        self.counter += 1
+        print self.counter
+        markers = state.markers
+        for marker in markers:
+            if marker.landmarkID.data=="Gate10":
+                if marker.markerID.data=="1":
+                    marker1 = [marker.x,marker.y]
+                if marker.markerID.data=="2":
+                    marker2 = [marker.x,marker.y]
+                if marker.markerID.data=="3":
+                    marker3 = [marker.x,marker.y]
+                if marker.markerID.data=="4":
+                    marker4 = [marker.x,marker.y] 
+            if marker.landmarkID.data=="Gate21":
+                if marker.markerID.data=="2":
+                    marker221 = [marker.x,marker.y]
+                if marker.markerID.data=="3":
+                    marker321 = [marker.x,marker.y]
+                if marker.markerID.data=="4":
+                    marker421 = [marker.x,marker.y]
+        try:
+            marker1,marker2,marker3,marker4
+            markersArr = np.array([marker1,marker2,marker3,marker4])
+        except:
+            print 'gate 10 not in view'
+        if self.counter>250:
+            try:
+                marker221,marker321,marker421
+                y_diff = marker321[1]-marker221[1]
+                marker121 = [marker421[0],marker421[0]-y_diff]
+                # print 'markers'
+                # print marker121,marker221,marker321,marker421
+                markersArr = np.array([marker221,marker121,marker321,marker421])
+            except:
+                print 'gate 21 not in view'      
+        if self.imu_cb_flag:
+            imu_state = [self.ang_x,self.ang_y,self.ang_z,self.lin_x,self.lin_y,self.lin_z]
+            nn_input = np.concatenate((markersArr.flatten(),np.array(imu_state)))
+            # print nn_input
+            outputs =  self.model.predict(nn_input.reshape(-1,14))
+            roll = outputs[0][0] 
+            pitch = outputs[0][1]
+            yaw = 0
+            thrust = outputs[0][2]
+            # print outputs
+            if abs(roll)==0.25:
+                roll = 0
+            if self.counter>320:
+                thrust=12.4
+                roll = 0
+            ang_scale = 1
+            thrust_scale = 1
+            target_attitude_thrust = RateThrust()
+            target_attitude_thrust.header.frame_id = "home"
+            target_attitude_thrust.header.stamp = rospy.Time.now()
+            target_attitude_thrust.angular_rates.x = roll
+            target_attitude_thrust.angular_rates.y = pitch
+            target_attitude_thrust.angular_rates.z = yaw
+            target_attitude_thrust.thrust.z = thrust
+            self.attitude_thrust_pub.publish(target_attitude_thrust)
+        # markersList.append([marker1,marker2,marker3,marker4])
+        # markersTime.append(t)
+
+        # except:
+        #     print 'marker missing'
+
+def main(args):
+    rospy.init_node('offb_node', anonymous=True)
+    ic = test()
+
+    try:
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
+
+if __name__ == '__main__':
+    main(sys.argv)
--- scripts/raw_dqn.py
+++ scripts/raw_dqn.py
@@ -25,7 +25,9 @@ from tf2_msgs.msg import TFMessage
 from flightgoggles.msg import IRMarkerArray
 import numpy as np
 import random
-
+from keras import layers, models, optimizers
+from keras import backend as K
+import time
 class rc_pub:
 	def __init__(self):
 
@@ -40,9 +42,18 @@ class rc_pub:
 		self.gate_number = 10
 		self.model = self._build_model()
 
+		self.roll = 0
+		self.pitch = 0
+		self.thrust = 0
+
+		self.roll_scale = 0.01
+		self.pitch_scale = 0.1
+		self.thrust_scale = 0.3
+
 		self.done = False
 		self.joy_cb_flag = False
-		self.batch_size = 4
+		self.imu_cb_flag = False
+		self.batch_size = 8
 
 		#CHANGE!!!
 		self.initGateDist = 4
@@ -59,7 +70,7 @@ class rc_pub:
 		self.prev_dist_to_gate = 26.5
 		self.reward_scale = 1
 
-		EPISODES = 2
+		EPISODES = 200
 
 		# Rate init
 		self.rate = rospy.Rate(20.0)  # MUST be more then 20Hz
@@ -75,6 +86,24 @@ class rc_pub:
 		self.rate_thrust_pub = rospy.Publisher("/uav/input/rateThrust", RateThrust, queue_size=10)
 		self.thrust_scale = 1
 		self.ang_scale = 1
+
+		#Test predict
+		# while not self.imu_cb_flag:
+		# 	target_rate_thrust = RateThrust()
+		# 	target_rate_thrust.header.frame_id = "home"
+		# 	target_rate_thrust.header.stamp = rospy.Time.now()
+		# 	target_rate_thrust.angular_rates.x = 1
+		# 	target_rate_thrust.angular_rates.y = 0
+		# 	target_rate_thrust.angular_rates.z = 0
+		# 	target_rate_thrust.thrust.z = 10.5
+		# 	self.rate_thrust_pub.publish(target_rate_thrust)
+		# 	self.sleep_rate.sleep()
+		# 	print 'waiting for imu data'
+		# imu_state = [self.ang_x,self.ang_y,self.ang_z,self.lin_x,self.lin_y,self.lin_z]
+		# next_state = np.concatenate((self.markersArr.flatten(),np.array(imu_state)))
+		# self.model.predict(next_state.reshape(-1,14))
+		
+
 		# while not rospy.is_shutdown():
 		# 	if(self.joy_cb_flag == True):
 		# 		target_rate_thrust = RateThrust()
@@ -87,56 +116,62 @@ class rc_pub:
 		# 		self.rate_thrust_pub.publish(target_rate_thrust)
 		# 	self.rate.sleep()
 		while not rospy.is_shutdown():
-		  for e in range(EPISODES):
 			print 'restarting'
 			self.crash_vehicle()
 			print 'crashed and reset'
-			# Wait for quad to reset
-			self.sleep_rate.sleep()
-			# state = env.reset()
-			# state = np.reshape(state, [1, state_size])
-			counter = 0
-			while 1:
-				print 'learning'
-				# self.crash_vehicle()
+			for e in range(EPISODES):
+				# Wait for quad to reset
+				self.sleep_rate.sleep()
+				# state = env.reset()
+				# state = np.reshape(state, [1, state_size])
+				counter = 0
+				while 1:
+					print 'learning'
+					# self.crash_vehicle()
 
-				imu_state = [self.ang_x,self.ang_y,self.ang_z,self.lin_x,self.lin_y,self.lin_z]
-				state = np.concatenate((self.markersArr.flatten(),np.array(imu_state)))
-				action = self.act(state,counter)
-				
-				# Sleeping to find the next state
-				self.rate.sleep()
-				reward = self.calc_reward()
-				imu_state = [self.ang_x,self.ang_y,self.ang_z,self.lin_x,self.lin_y,self.lin_z]
-				next_state = np.concatenate((self.markersArr.flatten(),np.array(imu_state)))
+					imu_state = [self.ang_x,self.ang_y,self.ang_z,self.lin_x,self.lin_y,self.lin_z]
+					state = np.concatenate((self.markersArr.flatten(),np.array(imu_state)))
+					outputs =  self.model.predict(state.reshape(-1,14))
+					print outputs
+					action = self.act(state,counter)
+					# print action
+					# print type(action[0])
+					self.do_action(action,counter)
+					# Sleeping to find the next state
+					self.rate.sleep()
+					reward = self.calc_reward()
+					imu_state = [self.ang_x,self.ang_y,self.ang_z,self.lin_x,self.lin_y,self.lin_z]
+					next_state = np.concatenate((self.markersArr.flatten(),np.array(imu_state)))
 
-				if self.crashFlag or not self.gateInViewFlag:
-					#Give negative reward
-					reward = -10
-					break
-				print reward
-				self.remember(state, action, reward, next_state)
-				# if len(self.memory) > self.batch_size:
-				# 	self.replay(self.batch_size)
-			#     env.render()
-			#     action = agent.act(state)
-			#     next_state, reward, done = step(action)
-			#     # next_state, reward, done, _ = env.step(action)
-			#     reward = reward if not done else -10
-			#     next_state = np.reshape(next_state, [1, state_size])
-			#     agent.remember(state, action, reward, next_state, done)
-			#     state = next_state
-			#     if done:
-			#         print("episode: {}/{}, score: {}, e: {:.2}"
-			#               .format(e, EPISODES, time, agent.epsilon))
-			#         break
-			#     if len(agent.memory) > batch_size:
-			#         agent.replay(batch_size)
-			#     self.rate.sleep()
-				if counter<5:
-					counter += 1
-			self.crashFlag = False
-		  self.rate.sleep()
+					if self.crashFlag or not self.gateInViewFlag:
+						#Give negative reward
+						print 'breaking!!!'
+						reward = -10
+						break
+					# print reward
+					self.remember(state, action, reward, next_state)
+					if len(self.memory) > self.batch_size:
+						self.replay(self.batch_size)
+				#     env.render()
+				#     action = agent.act(state)
+				#     next_state, reward, done = step(action)
+				#     # next_state, reward, done, _ = env.step(action)
+				#     reward = reward if not done else -10
+				#     next_state = np.reshape(next_state, [1, state_size])
+				#     agent.remember(state, action, reward, next_state, done)
+				#     state = next_state
+				#     if done:
+				#         print("episode: {}/{}, score: {}, e: {:.2}"
+				#               .format(e, EPISODES, time, agent.epsilon))
+				#         break
+				#     if len(agent.memory) > batch_size:
+				#         agent.replay(batch_size)
+				#     self.rate.sleep()
+					if counter<30:
+						counter += 1
+				self.crash_vehicle()
+				self.crashFlag = False
+			self.rate.sleep()
 
 	def joy_cb(self, state):
 		#print(state)
@@ -165,16 +200,39 @@ class rc_pub:
 	build network
 	'''
 	def _build_model(self):
-		# Neural Net for Deep-Q learning Model
-		model = load_model('my_model.h5')
-		# model._make_predict_function()
+		# # Neural Net for Deep-Q learning Model
+		# # model._make_predict_function()
+		# # model.add(Dense(24, input_dim=self.state_size, activation='relu'))
+		# # model.add(Dense(24, activation='relu'))
+		# # model.add(Dense(self.action_size, activation='linear'))
+		# # model.compile(loss='mse',
+		# #             optimizer=Adam(lr=self.learning_rate))
 		# model = Sequential()
-		# model.add(Dense(24, input_dim=self.state_size, activation='relu'))
-		# model.add(Dense(24, activation='relu'))
-		# model.add(Dense(self.action_size, activation='linear'))
+		# states = layers.Input(shape=(self.state_size,), name='states')
+		# net = layers.Dense(units=24, activation='relu')(states)
+		# # net = layers.BatchNormalization()(net)
+		# # net = layers.Dense(units=64, activation='relu')(net)
+		# # # net = layers.BatchNormalization()(net)
+		# net = layers.Dense(units=24, activation='relu')(net)
+		# raw_actions = layers.Dense(units=self.action_size, activation='sigmoid',name='raw_actions')(net)
 		# model.compile(loss='mse',
-		#             optimizer=Adam(lr=self.learning_rate))
+		# 			optimizer=Adam(lr=self.learning_rate))
+		states = layers.Input(shape=(self.state_size,), name='states')
+
+		# Add hidden layers
+		net = layers.Dense(units=32, activation='relu')(states)
+		net = layers.BatchNormalization()(net)
+		net = layers.Dense(units=64, activation='relu')(net)
+		net = layers.BatchNormalization()(net)
+		net = layers.Dense(units=32, activation='relu')(net)
+		raw_actions = layers.Dense(units=self.action_size, activation='sigmoid',name='raw_actions')(net)
+		# actions = layers.Lambda(lambda x: (x * [0.5,2,2]) + [-0.25,-1,9.8],name='actions')(raw_actions)
+		# Create Keras model
+		model = models.Model(inputs=states, outputs=raw_actions)
+		model.compile(loss='mean_squared_error', optimizer=Adam(lr=self.learning_rate))
+
 		return model
+
 
 	def collision_callback(self, msg):
 		self.crashFlag = True
@@ -243,86 +301,94 @@ class rc_pub:
 		for marker in markers:
 			if marker.landmarkID.data=="Gate10":
 				if marker.markerID.data=="1":
-					marker110 = [marker.x,marker.y]
+					marker1 = [marker.x,marker.y]
 				if marker.markerID.data=="2":
-					marker210 = [marker.x,marker.y]
+					marker2 = [marker.x,marker.y]
 				if marker.markerID.data=="3":
-					marker310 = [marker.x,marker.y]
+					marker3 = [marker.x,marker.y]
 				if marker.markerID.data=="4":
-					marker410 = [marker.x,marker.y]
-			if marker.landmarkID.data=="Gate21":
-				if marker.markerID.data=="2":
-					marker221 = [marker.x,marker.y]
-				if marker.markerID.data=="3":
-					marker321 = [marker.x,marker.y]
-				if marker.markerID.data=="4":
-					marker421 = [marker.x,marker.y]  
-			
+					marker4 = [marker.x,marker.y]       
 		try:
-			marker110,marker210,marker310,marker410
-			# print 'markers'
-			# print marker110,marker210,marker310,marker410
-			self.markersArr = np.array([marker110,marker210,marker310,marker410])
+			marker1,marker2,marker3,marker4
+			self.markersArr = np.array([marker1,marker2,marker3,marker4])
 			self.gateInViewFlag = True
 		except:
-			print 'gate 10 not in view'
-		try:
-			marker221,marker321,marker421
-			y_diff = marker321[1]-marker221[1]
-			marker121 = [marker421[0],marker421[0]-y_diff]
-			# print 'markers'
-			# print marker121,marker221,marker321,marker421
-			self.markersArr = np.array([marker221,marker121,marker321,marker421])
-			self.gateInViewFlag = True
-		except:
-			print 'gate 21 not in view'
-			# self.gateInViewFlag = False
+			print 'gate not in view'
+			self.gateInViewFlag = False
 	def remember(self, state, action, reward, next_state):
 		self.memory.append((state, action, reward, next_state))
 
+	def rand_outputs(self,counter):
+		roll = int(round(random.uniform(0, 1)))
+		pitch = int(round(random.uniform(0, 1)))
+		if counter<20:
+			thrust = 1
+		else:
+			thrust = int(round(random.uniform(0,1)))
+
+		return [roll,pitch,thrust]
 	def act(self, state,counter):
+		if np.random.rand() <= self.epsilon:
+			# The agent acts randomly
+			return self.rand_outputs(counter)
 		# if np.random.rand() <= self.epsilon:
 		#     return random.randrange(self.action_size)
-		print state
 		outputs =  self.model.predict(state.reshape(-1,14))
-		roll = outputs[0][0] 
-		pitch = outputs[0][1]
+		outputs = outputs[0]
+		rounded_ouputs = []
+		for output in outputs:
+			rounded_ouputs.append(int(round(output)))
+		return np.array(rounded_ouputs)  # returns action
+
+	def do_action(self,action,counter):
+		self.roll += self.roll_scale*round(action[0]) 
+		self.pitch += self.pitch_scale*round(action[1]) 
 		yaw = 0
-		thrust = outputs[0][2]
-		print outputs
+		self.thrust += self.thrust_scale*round(action[2]) 
+		# print [self.roll,self.pitch,self.thrust]
 		ang_scale = 1
 		thrust_scale = 1
 		target_rate_thrust = RateThrust()
 		target_rate_thrust.header.frame_id = "home"
 		target_rate_thrust.header.stamp = rospy.Time.now()
-		target_rate_thrust.angular_rates.x = roll
-		target_rate_thrust.angular_rates.y = pitch
+		target_rate_thrust.angular_rates.x = self.roll
+		target_rate_thrust.angular_rates.y = self.pitch
 		target_rate_thrust.angular_rates.z = yaw
-		if counter<5:
-			target_rate_thrust.thrust.z = thrust+1
+		if counter<20:
+			target_rate_thrust.thrust.z = 11
 		else:
-			target_rate_thrust.thrust.z = thrust
+			target_rate_thrust.thrust.z = self.thrust
 		self.rate_thrust_pub.publish(target_rate_thrust)
-		return outputs  # returns action
 
 	def replay(self, batch_size):
 		minibatch = random.sample(self.memory, batch_size)
 		for state, action, reward, next_state in minibatch:
-			# target = reward
-			# target = (reward + self.gamma *np.amax(self.model.predict(next_state.reshape(-1,14))[0]))
-			# target_f = self.model.predict(state.reshape(-1,14))
-			# target_f[0][action] = target
-			nEpochs = int(5*reward)
-			print 'reward'
-			print reward
-			print 'training epochs'
-			print nEpochs
-			if nEpochs>0:
-				print state.reshape(-1,14)
-				print action
-				self.model.fit(state.reshape(-1,14), action, epochs=nEpochs, verbose=0)
+			target = reward
+			target = reward + self.gamma * np.amax(self.model.predict(next_state.reshape(-1,14))[0])
+			target_f = self.model.predict(state.reshape(-1,14))
+			target_f[0][action] = target
+			# print target_f
+			self.model.fit(state.reshape(-1,14), target_f, epochs=1, verbose=0)
 		if self.epsilon > self.epsilon_min:
 			self.epsilon *= self.epsilon_decay
+	# def replay(self, batch_size):
+	# 	minibatch = random.sample(self.memory, batch_size)
+	# 	for state, action, reward, next_state in minibatch:
+	# 		# target = reward
+	# 		# target = (reward + self.gamma *np.amax(self.model.predict(next_state.reshape(-1,14))[0]))
+	# 		# target_f = self.model.predict(state.reshape(-1,14))
+	# 		# target_f[0][action] = target
+	# 		nEpochs = int(5*reward)
+	# 		print 'reward'
+	# 		print reward
+	# 		print 'training epochs'
+	# 		print nEpochs
+	# 		if nEpochs>0:
+	# 			print state.reshape(-1,14)
+	# 			print action
+	# 			self.model.fit(state.reshape(-1,14), action, epochs=nEpochs, verbose=0)
+	# 	if self.epsilon > self.epsilon_min:
+	# 		self.epsilon *= self.epsilon_decay
 
 	# def load(self, name):
 	#     self.model.load_weights(name)
